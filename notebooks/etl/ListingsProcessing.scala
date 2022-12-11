5d5ef6481827b580f3f0625b67a0352debb74140
smaglijkata2002@gmail.com <smaglijkata2002@gmail.com>
2022-12-11 16:33:33 +0000

    partition file to host and listings info

--- notebooks/etl/ListingsProcessing.scala
+++ notebooks/etl/ListingsProcessing.scala
@@ -2,61 +2,69 @@
 // MAGIC %md
 // MAGIC # Processing listings.csv
 // MAGIC 
-// MAGIC ...
+// MAGIC The file contains all the information concerning the AirBnb apartments.
 
 // COMMAND ----------
 
-import java.io.File
 import org.apache.spark.sql.functions._
 import spark.implicits._   
-
-val pathToFiles = "/FileStore/data/airbnb/"
-val nameListings = dbutils.widgets.get("fileName")
-
-
-// COMMAND ----------
-
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.types._
 
 
-def preprocessingListings(pathToFile: String, city: String): (DataFrame, DataFrame) ={
-  var listingsAirbnb = spark.read.format("csv").option("header", "true").option("sep", ",").load(pathToFile)
-  listingsAirbnb = listingsAirbnb
-    .withColumn("city", lit(city))
-    .withColumn("scraped_year", year(col("last_scraped")))
-    .withColumn("calculated_host_listings_count_private_rooms", col("calculated_host_listings_count_private_rooms").cast(IntegerType))
-    .withColumn("calculated_host_listings_count_entire_homes", col("calculated_host_listings_count_entire_homes").cast(IntegerType))
-    .withColumn("calculated_host_listings_count_shared_rooms", col("calculated_host_listings_count_shared_rooms").cast(IntegerType))
-    .withColumn("calculated_host_listings_count", col("calculated_host_listings_count").cast(IntegerType))
-    .withColumn("minimum_maximum_nights", col("minimum_maximum_nights").cast(IntegerType))
-    .withColumn("minimum_nights", col("minimum_nights").cast(IntegerType))
-    .withColumn("maximum_nights", col("maximum_nights").cast(IntegerType))
-    .withColumn("minimum_minimum_nights", col("minimum_minimum_nights").cast(IntegerType))
-    .withColumn("minimum_nights_avg_ntm", col("minimum_nights_avg_ntm").cast(IntegerType))
-    .withColumn("maximum_minimum_nights", col("maximum_minimum_nights").cast(IntegerType))
-    .withColumn("number_of_reviews", col("number_of_reviews").cast(IntegerType))
-    .withColumn("number_of_reviews_ltm", col("number_of_reviews_ltm").cast(IntegerType))
-    .withColumn("number_of_reviews_ltm", col("number_of_reviews_ltm").cast(IntegerType))
-  val sortedColumns = listingsAirbnb.columns.sorted.map(c=>col(c))
-  listingsAirbnb = listingsAirbnb.select(sortedColumns:_*)
-  listingsAirbnb = listingsAirbnb.withColumnRenamed("id","listing_id").dropDuplicates("listing_id")
-  val dropColumnSeq = Seq("host_name", "host_url", "host_since", "host_location", "host_about", "host_response_time", "host_response_rate", "host_acceptance_rate",
-    "host_is_superhost", "host_thumbnail_url", "host_picture_url", "host_neighbourhood", "host_listings_count",
-    "host_total_listings_count", "host_verifications", "host_has_profile_pic", "host_identity_verified")
-  (listingsAirbnb.drop(dropColumnSeq:_*), listingsAirbnb)
-}
+val pathToFile = "/FileStore/data/airbnb/London/london_listings_2022-09-10.csv"
 
 
 // COMMAND ----------
 
+import org.apache.spark.sql.types.IntegerType
+var listiningsAirbnb = spark.read.format("csv").option("header", "true").option("sep", ",").load(pathToFile)
 
-if (fileIsExists(nameListings)) {
-  val (berlinListiningsHost, berlinListinings) = preprocessingListings(pathToFiles+berlinListings, "Berlin")
-  berlinListinings.printSchema()
-//   display(berlinListinings.printSchema())
-}
+listiningsAirbnb = listiningsAirbnb
+  .withColumn("id", col("id").cast(IntegerType))
+  .withColumn("host_id", col("host_id").cast(IntegerType))
+  .withColumn("bedrooms", col("bedrooms").cast(IntegerType))
+  .withColumn("beds", col("beds").cast(IntegerType))
+  .withColumn("minimum_nights", col("minimum_nights").cast(IntegerType))
+  .withColumn("maximum_nights", col("maximum_nights").cast(IntegerType))
+  .withColumn("availability_30", col("availability_30").cast(IntegerType))
+  .withColumn("availability_60", col("availability_90").cast(IntegerType))
+  .withColumn("availability_90", col("availability_30").cast(IntegerType))
+  .withColumn("availability_365", col("availability_30").cast(IntegerType))
+  .withColumn("number_of_reviews", col("number_of_reviews").cast(IntegerType))
+  .withColumn("number_of_reviews_ltm", col("number_of_reviews_ltm").cast(IntegerType))
+  .withColumn("number_of_reviews_ltm", col("number_of_reviews_ltm").cast(IntegerType))
+  .withColumn("calculated_host_listings_count_private_rooms", col("calculated_host_listings_count_private_rooms").cast(IntegerType))
+  .withColumn("calculated_host_listings_count_entire_homes", col("calculated_host_listings_count_entire_homes").cast(IntegerType))
+  .withColumn("calculated_host_listings_count_shared_rooms", col("calculated_host_listings_count_shared_rooms").cast(IntegerType))
+  .withColumn("calculated_host_listings_count", col("calculated_host_listings_count").cast(IntegerType))
+  
+display(originValues)
+
 
 // COMMAND ----------
 
+val sortedColumns = listiningsAirbnb.columns.sorted.map(c=>col(c))
+listiningsAirbnb = listiningsAirbnb.select(sortedColumns:_*).withColumn("city", lit(pathToFile.split("/")(4)))
+listiningsAirbnb = listiningsAirbnb.withColumnRenamed("id","listing_id").dropDuplicates("listing_id")
 
+display(listiningsAirbnb)
+
+// COMMAND ----------
+
+val listiningsWithoutNull = listiningsAirbnb.filter(col("listing_id").isNotNull)
+
+display(listiningsWithoutNull)
+
+// COMMAND ----------
+
+val listiningsHost = listiningsWithoutNull.select("host_id", "host_name", "host_url", "host_since", "host_location", "host_about", "host_response_time", "host_response_rate", "host_acceptance_rate", "host_is_superhost", "host_thumbnail_url", "host_picture_url", "host_neighbourhood", "host_listings_count",
+    "host_total_listings_count", "host_verifications", "host_has_profile_pic", "host_identity_verified", "city")
+
+display(listiningsHost)
+
+// COMMAND ----------
+
+val outputPath = dbutils.widgets.get("outputPath")
+
+listiningsHost.write.options(Map("header"->"true", "escape"->"\"")).csv(outputPath + "host.csv")  
+listiningsWithoutNull.write.options(Map("header"->"true", "escape"->"\"")).csv(outputPath + "listings.csv") 
